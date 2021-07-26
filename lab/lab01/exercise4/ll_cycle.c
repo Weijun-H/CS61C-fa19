5617a937e5cf2cd2cd50bf9db0336dfbf09dd9ea
Weijun-H <huangweijun1001@gmail.com>
2021-07-26 17:03:14 +0000

    passed exercise4

--- lab/lab01/exercise4/ll_cycle.c
+++ lab/lab01/exercise4/ll_cycle.c
@@ -3,4 +3,18 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    if (head == NULL) return 0;
+    node *ptr_1 = head;
+    node *ptr_2 = head->next;
+    while (ptr_2 != NULL) {
+        if (ptr_2 == ptr_1) return 1;
+        ptr_1 = ptr_1->next;
+        for (int i = 0; i < 2; ++i){
+            ptr_2 = ptr_2->next;
+            if (ptr_2 == NULL) {
+                return 0;
+            }
+        }
+    }
+
 }
