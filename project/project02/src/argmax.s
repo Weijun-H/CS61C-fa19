36c12ef9c8119e7f3fbe6c3f5e5cf2f422fabfbf
Weijun-H <huangweijun1001@gmail.com>
2021-08-13 12:45:22 +0000

    Completed project02 exercise02

--- project/project02/src/argmax.s
+++ project/project02/src/argmax.s
@@ -15,20 +15,28 @@
 #   this function terminates the program with error code 32
 # =================================================================
 argmax:
+    li t0, 0
+    li t4, 1
+    ble t4, a1, no_exception
+    li a1, 32
+    j exit2
 
-    # Prologue
-
+no_exception:
+    lw t1, 0(a0) # t1 = a[0]
+    li t3, 0 # t2 = index of the largest
 
 loop_start:
-
+    lw t2, 0(a0) # t2 = a[i]
+    bge t1, t2, loop_continue # if temp > a[i] continue 
+    add t1, t2, x0
+    add t3, x0, t0
 
 loop_continue:
-
+    addi a0, a0, 4
+    addi t0, t0, 1
+    bge	t0, a1, loop_end # if t0 > the number of the array
+    j loop_start
 
 loop_end:
-    
-
-    # Epilogue
-
-
-    ret
+    add a0, t3, x0
+    ret    
